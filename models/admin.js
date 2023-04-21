4967b244c791bf193b5b49cfd6d50dfb8c6ea293
pranav-ml <srivatsavpranav@gmail.com>
2023-04-21 16:33:32 +0530

    first commit

---
+++ models/admin.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const passportLocalMongoose = require("passport-local-mongoose");
+const Schema = mongoose.Schema;
+
+const adminSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    }
+});
+
+adminSchema.plugin(passportLocalMongoose);
+
+module.exports = mongoose.model("Admin", adminSchema);
